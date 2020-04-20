b375f5f0acac949387df95611f764242e6c23fb7
Jędrzej Kowaliński <jjkowalinski@yahoo.com>
2020-04-20 12:07:23 +0200

    publish previous made app

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const AboutPage = () => (
+  <div>
+    <h2>About</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum est tempore
+      sit ab, dolore error non exercitationem ipsam dicta beatae corrupti maxime
+      nemo numquam quas. Consequatur deleniti amet blanditiis nihil.
+    </p>
+  </div>
+);
+
+export default AboutPage;
---
+++ src/redux/actions/courseActions.js
@@ -0,0 +1,57 @@
+import * as types from "./actionTypes";
+import * as courseAPI from "../../api/courseApi";
+import { beginApiCall, apiCallError } from "./apiStatusActions";
+
+export function loadCourseSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+export function deleteCourseOptimistic(course) {
+  return { type: types.DELETE_COURSE_OPTIMISTIC, course };
+}
+
+export function loadCourses() {
+  return (dispatch) => {
+    dispatch(beginApiCall());
+    return courseAPI
+      .getCourses()
+      .then((courses) => {
+        dispatch(loadCourseSuccess(courses));
+      })
+      .catch((error) => {
+        throw error;
+      });
+  };
+}
+
+export function saveCourse(course) {
+  return (dispatch) => {
+    dispatch(beginApiCall());
+    return courseAPI
+      .saveCourse(course)
+      .then((savedCourse) => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch((error) => {
+        dispatch(apiCallError(error));
+        throw error;
+      });
+  };
+}
+
+export function deleteCourse(course) {
+  return (dispatch) => {
+    dispatch(deleteCourseOptimistic(course));
+    return courseAPI.deleteCourse(course.id);
+  };
+}
